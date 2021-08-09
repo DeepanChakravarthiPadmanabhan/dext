7c0cdba26978572a8acae6512e3f3f712d05c073
deepan <deepangrad@gmail.com>
2021-08-09 13:15:07 +0200

    first commit to master

---
+++ README.md
@@ -0,0 +1,9 @@
+# DExT: Detector Explanation Toolkit
+
+## Installation
+```
+pip install -e .
+```
+
+## References
+[1] Octavio Arriaga, Matias Valdenegro-Toro, Mohandass Muthuraja, Sushma Devaramani, and Frank Kirchner. "Perception for Autonomous Systems (PAZ)." arXiv preprint arXiv:2010.14541. 2020.
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+
+__version__ = "0.1"
+
+if "VERSION" in os.environ:
+    BUILD_NUMBER = os.environ["VERSION"].rsplit(".", 1)[-1]
+else:
+    BUILD_NUMBER = os.environ.get("BUILD_NUMBER", "dev")
+
+dependencies = [
+    "numpy",
+    "opencv-python",
+    "tensorflow",
+    "pypaz",
+]
+
+setup(
+    name="dext",
+    version="{0}.{1}".format(__version__, BUILD_NUMBER),
+    description="A package to explain object detectors",
+    author="Deepan Chakravarthi Padmanabhan",
+    install_requires=dependencies,
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points=dict(
+        console_scripts=[
+            "dext_explainer=dext.explainer:explain",
+        ]
+    ),
+    python_requires=">=3.6,<=3.9",
+)
